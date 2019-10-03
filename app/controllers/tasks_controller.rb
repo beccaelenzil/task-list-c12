3aab07522428068c1d8865802927c2b94c2e5f9b
beccaelenzil <beccaelenzil@gmail.com>
2019-10-03 16:31:20 -0700

    Baseline and Wave 1, actions: index, show, restful routes, task model

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+TASKS = [{name: "learn rails", description: "learn rails", due_date: "01.01.2020"}]
+
+class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+
+  def show
+    #task_id = params[:id]
+    #@task = Task.find_by(id: task_id)
+    @task = TASKS[0]
+  end
+end
