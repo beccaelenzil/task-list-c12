c585341f09de4bf5d948745f7136210eff7f6522
beccaelenzil <beccaelenzil@gmail.com>
2019-10-03 16:36:21 -0700

    Complete Wave 1: Task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ TASKS = [{name: "learn rails", description: "learn rails", due_date: "01.01.2020
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
