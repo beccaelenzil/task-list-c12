e4cbbfdd0772146c98996f902894e3a443c21fc8
beccaelenzil <beccaelenzil@gmail.com>
2019-10-04 11:27:14 -0700

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root 'tasks#index'
+    #resources :tasks, except :index
     # Routes that oper'ate on the task collection
     root 'tasks#index'
     get '/tasks', to: 'tasks#index', as: 'tasks'
@@ -11,6 +14,7 @@ Rails.application.routes.draw do
     get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
     patch '/tasks/:id', to: 'tasks#update'
     delete '/tasks/:id', to: 'tasks#destroy'
+    
 
     patch '/tasks/:id/mark_incomplete', to: 'tasks#mark_incomplete', as: 'mark_incomplete'
     patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
