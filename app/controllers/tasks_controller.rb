a958e032141d0a7eb6ed880a0ce9953bf9212d30
beccaelenzil <beccaelenzil@gmail.com>
2019-10-03 17:20:43 -0700

    edit, update, destroy actions and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,12 +23,46 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(name: params[:task][:name], description: params[:task][:description], due_date: params[:task][:due_date]) #instantiate a new task
     if @task.save # save returns true if the database insert succeeds
-      redirect_to task_path(@task) # go to the index so we can see the book in the list
+      redirect_to task_path(@task) # go to the index so we can see the task in the list
       return
     else # save failed :(
-      render :new # show the new book form view again
+      render :new # show the new task form view again
       return
     end
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    @task.update(name: params[:task][:name], description: params[:task][:description], due_date: params[:task][:due_date])
+
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :edit # show the new task form view again
+      return
+    end
+  end
+
+  def destroy
+    task_id = params[:id]
+    @task = Task.find_by(id:task_id)
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+
 
 end
